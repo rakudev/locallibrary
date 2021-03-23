51e25ea05f6e6b1c5ed26195531793e34e65ceb9
ratchek <git@ratchek.com>
2021-03-23 13:52:33 -0400

    Finish part 6

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
 def index(request):
     """View function for home page of site."""
 
